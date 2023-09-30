2f6137a441fa215f6a53f846aa311c27d997379e
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-30 08:47:56 +0300

    Fetch to see all character names in a div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+function handleDOMContentLoaded() {
+  // grab elements
+  const characterBar = document.querySelector("#character-bar");
+  const detailedInfo = document.querySelector("#detailed-info");
+  const image = document.querySelector("#image");
+  const voteCount = document.querySelector("#vote-count");
+  const votesForm = document.querySelector("#votes-form");
+  const votes = document.querySelector("#votes");
+  const resetBtn = document.querySelector("#reset-btn");
+
+  //   fetch API
+  fetch("http://localhost:3000/characters")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      //   see all character names in a div
+      data.map((character) => {
+        const spanCharacter = document.createElement("span");
+        spanCharacter.textContent = character.name;
+        characterBar.appendChild(spanCharacter);
+      });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
