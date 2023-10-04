01b81cb6d2cb4153ae1ac659253c7a8122fb4c7f
githinjisamson1 <githinjisamson148@gmail.com>
2023-10-04 17:34:14 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,48 +1,62 @@
-// Your code here
+function handleVotesFormSubmission(e, characterVotes) {
+  // prevent refresh default behavior
+  e.preventDefault();
 
-function handleDOMContentLoaded() {
-  // grab elements
-  const characterBar = document.querySelector("#character-bar");
-  const detailedInfo = document.querySelector("#detailed-info");
-  const name = document.querySelector("#name");
-  const image = document.querySelector("#image");
-  const voteCount = document.querySelector("#vote-count");
+  const votesValue = document.querySelector("#votes").value;
+  characterVotes.innerHTML = votesValue;
+
+  //   clear form values upon submission
+  e.target.reset();
+}
+
+function handleSpanCharacterClick(e, animal) {
+  const characterName = document.querySelector("#name");
+  const img = document.querySelector("#image");
+  const characterVotes = document.querySelector("#vote-count");
+
   const votesForm = document.querySelector("#votes-form");
-  const votes = document.querySelector("#votes");
-  const resetBtn = document.querySelector("#reset-btn");
 
-  //   fetch API - all
+  //   manipulate dom
+  characterName.innerHTML = animal.name;
+  img.src = animal.image;
+  characterVotes.innerHTML = animal.votes;
+
+  //   submitting form
+  votesForm.addEventListener("submit", (e) => {
+    handleVotesFormSubmission(e, characterVotes);
+  });
+}
+
+function seeAllCharacters(data) {
+  // iterate received data
+  data.forEach((animal) => {
+    const characterBar = document.querySelector("#character-bar");
+    const spanCharacter = document.createElement("span");
+
+    // manipulate dom
+    spanCharacter.innerHTML = animal.name;
+    characterBar.appendChild(spanCharacter);
+
+    // clicking one character
+    spanCharacter.addEventListener("click", (e) => {
+      handleSpanCharacterClick(e, animal);
+    });
+  });
+}
+
+function handleDOMContentLoaded(e) {
+  // fetch API - all
   fetch("http://localhost:3000/characters")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-
-      //   see all character names in a div
-      data.map((character) => {
-        const spanCharacter = document.createElement("span");
-        spanCharacter.textContent = character.name;
-        characterBar.appendChild(spanCharacter);
-
-        // display character details
-        spanCharacter.addEventListener("click", function (e) {
-          //   console.log(e.target);
-
-          name.textContent = character.name;
-          image.src = character.image;
-          voteCount.textContent = character.votes;
-
-          //   display number of votes from input field
-          votesForm.addEventListener("submit", function (e) {
-            //   console.log(e.target);
-
-            e.preventDefault();
-            voteCount.textContent = votes.value;
-          });
-        });
-      });
+      seeAllCharacters(data);
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 }
 
+// wait HTML to load first
 document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
